50973b339a6d0fb9972ec2347bbe1332c1a1d348
katmpatz <32817352+katmpatz@users.noreply.github.com>
2018-09-26 13:28:02 +0300

    Fix text editor issues

--- assets/js/texteditor.js
+++ assets/js/texteditor.js
@@ -1,4 +1,52 @@
-var quill = new Quill('.editor-container', {
+var quill = new Quill('#editor-container1', {
+modules: {
+toolbar: [
+[{ header: [1, 2, false] }],
+['bold', 'italic', 'underline'],
+['image']
+]
+},
+placeholder: 'Write your answer...',
+theme: 'snow'  // or 'bubble'
+});
+
+var quill = new Quill('#editor-container2', {
+modules: {
+toolbar: [
+[{ header: [1, 2, false] }],
+['bold', 'italic', 'underline'],
+['image']
+]
+},
+placeholder: 'Write your answer...',
+theme: 'snow'  // or 'bubble'
+});
+
+var quill = new Quill('#editor-container3', {
+modules: {
+toolbar: [
+[{ header: [1, 2, false] }],
+['bold', 'italic', 'underline'],
+['image']
+]
+},
+placeholder: 'Write your answer...',
+theme: 'snow'  // or 'bubble'
+});
+
+var quill = new Quill('#editor-container4', {
+modules: {
+toolbar: [
+[{ header: [1, 2, false] }],
+['bold', 'italic', 'underline'],
+['image']
+]
+},
+placeholder: 'Write your answer...',
+theme: 'snow'  // or 'bubble'
+});
+
+var quill = new Quill('#editor-container5', {
 modules: {
 toolbar: [
 [{ header: [1, 2, false] }],
